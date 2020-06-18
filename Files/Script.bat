fb9ee6d26fe4ff356b4c65d83108b1909dc95ad5
itrepo01 <itrepo01@gmail.com>
2020-06-18 15:52:04 -0700

    Add Files sample

---
+++ Files/Script.bat
@@ -0,0 +1,10 @@
+echo off
+rem  Script to create test folder
+rem  ----------------------------
+echo Creating test directory
+mkdir Test
+echo Copying files to test directory
+copy *.txt Test\
+echo Test directory has been created
+pause
+ 
